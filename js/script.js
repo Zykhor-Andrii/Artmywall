3d0221a7b9c102c049c3d31673670178d7a94000
Zykhor <zuxoran@gmail.com>
2024-05-21 17:57:38 +0300

    develop

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// import Swiper from 'swiper/bundle';
+
+// import { Navigation, Pagination } from 'swiper/modules';
+
+// console.log('ddddddddddd');
+
+// const swiper = new Swiper('.mySwiper', {
+// // Optional parameters
+// direction: 'vertical',
+// loop: true,
+
+// // If we need pagination
+// pagination: {
+// el: '.swiper-pagination',
+// },
+
+// // Navigation arrows
+// navigation: {
+// nextEl: '.swiper-button-next',
+// prevEl: '.swiper-button-prev',
+// },
+
+// // And if we need scrollbar
+// scrollbar: {
+// el: '.swiper-scrollbar',
+// },
+// });
